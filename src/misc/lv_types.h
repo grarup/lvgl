49256b8e4d10f8a840b38fde27e8b29f3966492d
Peter Grarup <pegp@dgs-diagnostics.com>
2021-10-04 12:30:17 +0200

    gnu_printf addef of gnu  > 4.4

--- src/misc/lv_types.h
+++ src/misc/lv_types.h
@@ -7,7 +7,8 @@
 #define LV_TYPES_H
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 /*********************
@@ -27,28 +28,29 @@ extern "C" {
 #define LV_ARCH_64
 
 // Otherwise use compiler-dependent means to determine arch size
-#elif defined(_WIN64) || defined(__x86_64__) || defined(__ppc64__) || defined (__aarch64__)
+#elif defined(_WIN64) || defined(__x86_64__) || defined(__ppc64__) || defined(__aarch64__)
 #define LV_ARCH_64
 
 #endif
 
-/**********************
+  /**********************
  *      TYPEDEFS
  **********************/
 
-/**
+  /**
  * LVGL error codes.
  */
-enum {
+  enum
+  {
     LV_RES_INV = 0, /*Typically indicates that the object is deleted (become invalid) in the action
                        function or an operation was failed*/
     LV_RES_OK,      /*The object is valid (no deleted) after the action*/
-};
-typedef uint8_t lv_res_t;
+  };
+  typedef uint8_t lv_res_t;
 
 #if defined(__cplusplus) || __STDC_VERSION__ >= 199901L
-// If c99 or newer,  use the definition of uintptr_t directly from <stdint.h>
-typedef uintptr_t lv_uintptr_t;
+  // If c99 or newer,  use the definition of uintptr_t directly from <stdint.h>
+  typedef uintptr_t lv_uintptr_t;
 
 #else
 
@@ -61,24 +63,28 @@ typedef uint32_t lv_uintptr_t;
 
 #endif
 
-/**********************
+  /**********************
  * GLOBAL PROTOTYPES
  **********************/
 
-/**********************
+  /**********************
  *      MACROS
  **********************/
 
-#define LV_UNUSED(x) ((void) x)
+#define LV_UNUSED(x) ((void)x)
 
-#define _LV_CONCAT(x, y) x ## y
-#define LV_CONCAT(x, y) _LV_CONCAT(x, y)
+#define _LV_CONCAT(x, y) x##y
+#define LV_CONCAT(x, y)  _LV_CONCAT(x, y)
 
-#define _LV_CONCAT3(x, y, z) x ## y ## z
-#define LV_CONCAT3(x, y, z) _LV_CONCAT3(x, y, z)
+#define _LV_CONCAT3(x, y, z) x##y##z
+#define LV_CONCAT3(x, y, z)  _LV_CONCAT3(x, y, z)
 
-#if (defined(__clang__) || defined(__GNUC__) || defined(__GNUG__)) && !defined(PYCPARSER)
-#define LV_FORMAT_ATTRIBUTE(fmtstr, vararg) __attribute__ ((format(printf, fmtstr, vararg)))
+#if defined(PYCPARSER)
+#define LV_FORMAT_ATTRIBUTE(fmtstr, vararg)
+#elif defined(__GNUC__) && ((__GNUC__ == 4 && __GNUC_MINOR__ >= 4) || __GNUC__ > 4)
+#define LV_FORMAT_ATTRIBUTE(fmtstr, vararg) __attribute__((format(gnu_printf, fmtstr, vararg)))
+#elif (defined(__clang__) || defined(__GNUC__) || defined(__GNUG__))
+#define LV_FORMAT_ATTRIBUTE(fmtstr, vararg) __attribute__((format(printf, fmtstr, vararg)))
 #else
 #define LV_FORMAT_ATTRIBUTE(fmtstr, vararg)
 #endif
